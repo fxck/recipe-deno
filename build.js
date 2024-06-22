31b045cb56599dc038b8b18aefa9f21f97610093
Ales Rechtorik <foxxcz@gmail.com>
2024-06-22 21:07:15 +0200

    asd

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.21.5/mod.js";
-import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.6.0/mod.ts";
+import { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.6.0/mod.ts";
 
 await esbuild.build({
   entryPoints: ["./src/main.ts"],
   bundle: true,
   outfile: "./dist/bundle.js",
-  plugins: [...denoPlugins({
+  plugins: [denoPlugin({
     importMapURL: new URL("./import_map.json", import.meta.url)
   })],
   format: "esm",
