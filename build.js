ac60e44641b530ccd24bb4ceb9bb2aa288db7517
Ales Rechtorik <foxxcz@gmail.com>
2024-06-22 20:56:24 +0200

    asd

--- build.js
+++ build.js
@@ -1,11 +1,11 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.21.5/mod.js";
-import { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
+import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.9.0/mod.ts";
 
 await esbuild.build({
   entryPoints: ["./src/main.ts"],
   bundle: true,
   outfile: "./dist/bundle.js",
-  plugins: [denoPlugin()],
+  plugins: denoPlugins(),
   format: "esm",
   platform: "neutral",
 });
